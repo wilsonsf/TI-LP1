9ccd5633c3041459efc5bc8723bdd4a05415b8f3
Wilson de Farias <wilsonsf1@gmail.com>
2015-10-13 11:12:16 -0300

    Main com menu e limpeza de tela.

--- Aula21/21-01/main.cpp
+++ Aula21/21-01/main.cpp
@@ -1,5 +1,8 @@
 /*
-  Faça um programa usando as rotinas genéricas de manipulação de deque, que permita manipular um deque de números reais. Implemente um menu, contendo as seguintes opções para o usuário:
+  Faça um programa usando as rotinas genéricas de manipulação de deque, que
+  permita manipular um deque de números reais. Implemente um menu, contendo 
+  as seguintes opções para o usuário:
+
   (1) Inserir um valor no início do deque;
   (2) Inserir um valor no final do deque;
   (3) Retirar um valor do início do deque;
@@ -11,8 +14,50 @@
 */
 
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
 int main () {
+
+  int opcao = 0;
+
+  do {
+    system("clear");
+    bool vaiSair = false;
+    cout << "(1) Inserir um valor no início do deque" << endl;
+    cout << "(2) Inserir um valor no final do deque" << endl;
+    cout << "(3) Retirar um valor do início do deque" << endl;
+    cout << "(4) Retirar um valor do final do deque" << endl;
+    cout << "(5) Listar o conteúdo do deque na tela" << endl;
+    cout << "(6) Remover todos os valores do deque" << endl;
+    cout << "(7) Fim da execução do programa" << endl;
+    cout << "Escolha uma opção: ";
+    cin >> opcao;
+
+    switch (opcao) {
+      case 1:
+      break;
+      case 2:
+      break;
+      case 3:
+      break;
+      case 4:
+      break;
+      case 5:
+      break;
+      case 6:
+      break;
+      case 7:
+        vaiSair = true;
+        break;
+      default:
+        cout << "Opcao inválida, tente novamente." << endl;
+    }
+
+    if (vaiSair)
+      break;
+
+  } while (opcao != 7);
 
   return 0;
 }

64800e2b7f9d5ab94c7518ebf1df2ace570224d8
Wilson de Farias <wilsonsf1@gmail.com>
2015-10-16 13:33:52 -0300

    21-03: Rotina de chamada e testes criada.

--- Aula21/21-03/main.cpp
+++ Aula21/21-03/main.cpp
@@ -8,8 +8,25 @@
 */
 
 #include <iostream>
+#include <cstring>
 
+using namespace std;
 int main () {
+
+
+  char entrada[256];
+  while (true) {
+
+    cin.getline(entrada,256);
+
+    cout << entrada << endl;
+    for (int i = 0; i < strlen(entrada); i++) {
+      // Inserir os elementos na pilha
+    }
+
+    // chamar o teste de simetria a partir da estrutura de dados
+    cout << entrada << (true ? "" : " nao ") << " eh simetrico" << endl;
+  }
 
   return 0;
 }

9f8ce4eb894fe9b10647d3187a090e754a5a9f4f
Wilson de Farias <wilsonsf1@gmail.com>
2015-10-20 12:17:29 -0300

    23-01: Cabecalho descritivo da atividade adicionado

---
+++ Aula23/23-01/main.cpp
@@ -0,0 +1,15 @@
+/*
+
+Faça um programa que replica o deque implementado no exercício 1 da aula 21.
+No entanto, ao invés do deque ser exclusivo para o uso de números reais, 
+generalize-o utilizando um tipo abstrato através do conceito de templates. 
+O deque deve ser uma classe.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main () {
+  return 0;
+}
